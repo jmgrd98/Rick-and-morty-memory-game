a599c44800fe36e918d8e21db6ff2244ea76b484
João Marcelo Dantas <jmgrd98@gmail.com>
2022-08-13 20:11:23 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const input = document.getElementById('input');
 const button = document.getElementById('button')
+const form = document.getElementById('form')
+
 
 const validateInput = ({target}) => {
     if(target.value.length > 2){
         button.removeAttribute('disabled')
+        return
     }
+    button.setAttribute('disabled', '')
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('player', input.value)
+    window.location = 'game.html'
 }
 
 input.addEventListener('input', validateInput)
+form.addEventListener('submit', handleSubmit)
